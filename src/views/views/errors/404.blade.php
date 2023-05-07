86afeb8e99a4ad5536cd7e16935dce70ae5ddd8a
571816409@qq.com <571816409@qq.com>
2023-05-07 19:23:05 +0800

    给你更新数据

---
+++ src/views/views/errors/404.blade.php
@@ -0,0 +1,16 @@
+@push('title')
+<title>404</title>
+@endpush
+@extends('layout.layer')
+@section('content')
+    <div class="layadmin-tips">
+        <i class="layui-icon" face>&#xe664;</i>
+        <div class="layui-text">
+            <h1>
+                <span class="layui-anim layui-anim-loop layui-anim-">4</span>
+                <span class="layui-anim layui-anim-loop layui-anim-rotate">0</span>
+                <span class="layui-anim layui-anim-loop layui-anim-">4</span>
+            </h1>
+        </div>
+    </div>
+@endsection
---
+++ src/views/views/layout/header.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    @stack('title')
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <link rel="stylesheet" href="/static/css/layui.css">
+    @stack('style')
+    @stack('style2')
+    <script src="/static/layui.js"></script>
+</head>
+
+<body>
